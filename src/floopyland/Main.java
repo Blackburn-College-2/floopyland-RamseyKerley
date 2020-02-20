c218ef084cba593e3d45953f7c11d8e10fc1f323
RamseyKerley <kerleyramsey@gmail.com>
2020-02-20 13:50:47 -0600

    started

--- src/floopyland/Main.java
+++ src/floopyland/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package emptyjavaproject;
+package floopyland;
 
 /**
  *
@@ -16,6 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
